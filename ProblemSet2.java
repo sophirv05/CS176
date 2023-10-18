7dcf8345d3cb374f20e434a685fb21819c5906cb
Sophia Ramirez Velandia <114093203+sophirv05@users.noreply.github.com>
2023-10-18 10:23:13 -0400

    Problem Set 2

--- ProblemSet2.java
+++ ProblemSet2.java
@@ -76,6 +76,34 @@ public class ProblemSet2 {
 	      System.out.println("Name: " + tambourine.getName());
 	      System.out.println("Type: " + tambourine.getType());
 	      System.out.println("Number of Keys or Strings: " + tambourine.getNumberOfKeysorStrings());
+	      System.out.println("");
+	      
+	      // Invalid value for name 
+	      MusicalInstrument checkname = new MusicalInstrument(2, "String", "");
+	      System.out.println("Unknown name:");
+	      System.out.println("Name: " + checkname.getName());
+	      System.out.println("Type: " + checkname.getType());
+	      System.out.println("Number of Keys or Strings: " + checkname.getNumberOfKeysorStrings());
+	      System.out.println("");
+	      
+	     // Invalid value for type
+	      MusicalInstrument checktype = new MusicalInstrument(2, "Something", "Cello");
+	      System.out.println("Unknown type:");
+	      System.out.println("Name: " + checktype.getName());
+	      System.out.println("Type: " + checktype.getType());
+	      System.out.println("Number of Keys or Strings: " + checktype.getNumberOfKeysorStrings());
+	      System.out.println("");
+	      
+	      // Invalid value for number of keys or string
+	      MusicalInstrument checkNumberOfKeysorStrings = new MusicalInstrument(-50, "String", "Cello");
+	      System.out.println("Unknown number of keys or string:");
+	      System.out.println("Name: " + checkNumberOfKeysorStrings.getName());
+	      System.out.println("Type: " + checkNumberOfKeysorStrings.getType());
+	      System.out.println("Number of Keys or Strings: " + checkNumberOfKeysorStrings.getNumberOfKeysorStrings());
+	      System.out.println("");
+	      
+	      
+
 	}
 
 }
